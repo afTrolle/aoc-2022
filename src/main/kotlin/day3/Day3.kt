36c95e41352e83a3e6cb91dad07dfc35752885fb
Alexander af Trolle <alexander.af.trolle@gmail.com>
2022-12-02 10:46:39 +0100

    Prepare day 3

---
+++ src/main/kotlin/day3/Day3.kt
@@ -0,0 +1,25 @@
+package day3
+
+import Day
+import day2.Day2.Hand.*
+import day2.Day2.Score.*
+import ext.reverse
+
+fun main() {
+    Day3("Day03").solve()
+}
+
+class Day3(input: String) : Day<Any>(input) {
+
+    override fun parse(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun part1(input: Any): Any? {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: Any): Any? {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/test/kotlin/day3/Day3Test.kt
@@ -0,0 +1,14 @@
+package day3
+
+import org.junit.jupiter.api.Test
+
+class Day3Test {
+
+    @Test
+    fun parseInput() {
+        val day = Day3("Day03_sample")
+        val result = day.parse()
+
+
+    }
+}

13d8ac0643ef752db261c42cb47d9fc7ff6dd7d0
Alexander af Trolle <alexander.af.trolle@gmail.com>
2022-12-03 12:53:36 +0100

    Prep day 4

---
+++ src/main/kotlin/day4/Day4.kt
@@ -0,0 +1,22 @@
+package day4
+
+import Day
+
+fun main() {
+    Day4("Day04").solve()
+}
+
+class Day4(input: String) : Day<Any>(input) {
+
+    override fun parse(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun part1(input: Any): Any? {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: Any): Any? {
+        TODO("Not yet implemented")
+    }
+}

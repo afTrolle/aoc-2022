83e698a6d47469ecafa49e309f63d96712f195bc
Alexander af Trolle <alexander.af.trolle@gmail.com>
2022-12-23 16:30:21 +0100

    Day 12 bfs

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -11,141 +11,103 @@ fun main() {
     Day12("Day12").solve()
 }
 
-class Day12(input: String) : Day<List<List<Char>>>(input) {
+data class Vertex(
+    val y: Int,
+    val x: Int,
+    val encodedAltitude: Char,
+) {
+    val altitude: Int = Day12.convertChar[encodedAltitude]!!
+    var left: Vertex? = null
+    var right: Vertex? = null
+    var up: Vertex? = null
+    var down: Vertex? = null
+    val neighbors get() = listOfNotNull(left, right, up, down)
+}
 
-    // TODO change to Map with nodes,
-    data class Point(
-        val y: Int,
-        val x: Int,
-        val z: Int,
-    )
+fun Vertex.pointIsValidHeight(current: Vertex): Vertex? = takeIf {
+    altitude <= current.altitude + 1
+}
 
-    fun Point.pointIsValidHeight(other: Point): Point? = takeIf { (z - other.z).absoluteValue <= 1 }
-
-    private val range: Map<Char, Int> =
-        ('a'..'z').map { it to (it - 'a') }.plus('S' to 0).plus('E' to ('z' - 'a')).toMap()
-    private val start = 'S'
-    private val end = 'E'
-
-    override fun parseInput(): List<List<Char>> {
-        return inputByLines.map { it.toList() }
-    }
-
-    data class Params(
-        val current: Point, val end: Point, val map: List<List<Point>>, val visited: Set<Point>
-    )
-
-    fun getNextPositon(
-        current: Point,
-        end: Point,
-        map: List<List<Point>>
-    ): List<Point> {
-        val down = map.getOrNull(current.y + 1)?.getOrNull(current.x)?.pointIsValidHeight(current)
-        val up = map.getOrNull(current.y - 1)?.getOrNull(current.x)?.pointIsValidHeight(current)
-        val left = map.getOrNull(current.y)?.getOrNull(current.x - 1)?.pointIsValidHeight(current)
-        val right = map.getOrNull(current.y)?.getOrNull(current.x + 1)?.pointIsValidHeight(current)
-
-        // c 0 0   - 0
-        // 0 0 y   - 1
-
-        // prio-direction.
-        val yDiff = current.y - end.y
-        val xDiff = current.x - end.x
-        return buildList {
-            if (xDiff.absoluteValue > yDiff.absoluteValue) {
-                if (xDiff < 0) right?.let { add(it) } else left?.let { add(it) }
-                if (yDiff < 0) down?.let { add(it) } else up?.let { add(it) }
-
-                if (yDiff < 0) up?.let { add(it) } else down?.let { add(it) }
-                if (xDiff < 0) left?.let { add(it) } else right?.let { add(it) }
-            } else {
-                if (yDiff < 0) down?.let { add(it) } else up?.let { add(it) }
-                if (xDiff < 0) right?.let { add(it) } else left?.let { add(it) }
-
-                if (xDiff < 0) left?.let { add(it) } else right?.let { add(it) }
-                if (yDiff < 0) up?.let { add(it) } else down?.let { add(it) }
-            }
-        }
-    }
-
-    val depth = DeepRecursiveFunction<Params, Int?> { param ->
-        param.run {
-            val size = visited.size
-            val minSteps = (current.y - end.y).absoluteValue + (current.x - end.x).absoluteValue
-            if (current == end) {
-                globalMin.update { currentMin ->
-                    if (currentMin > size) size else currentMin
-                }
-                size
-            } else if (size + minSteps >= globalMin.value) {
-                null
-            } else if (visited.contains(current)) {
-                null
-            } else {
-                val updatedVisited = visited.plus(current)
-                getNextPositon(current, end, map).mapNotNull { next ->
-                    callRecursive(param.copy(current = next, visited = updatedVisited))
-                }.minOrNull()
-            }
-        }
-    }
-
-
-    private val globalMin = MutableStateFlow(Int.MAX_VALUE)
-
-    @OptIn(DelicateCoroutinesApi::class)
-    suspend fun findEnd(current: Point, end: Point, map: List<List<Point>>, visited: Set<Point>): Int? {
-        val minSteps = (current.y - end.y).absoluteValue + (current.x - end.x).absoluteValue
-        return if (current == end) {
-            println(visited.size)
-            visited.size.also { size ->
-                globalMin.update { currentMin ->
-                    if (currentMin > size) size else currentMin
-                }
-            }
-        } else if (visited.size + minSteps >= globalMin.value) {
-            null
-        } else if (visited.contains(current)) {
-            null
-        } else {
-            val updatedVisited = visited.plus(current)
-            getNextPositon(current, end, map).map { next ->
-                GlobalScope.async(Dispatchers.Default) {
-                    findEnd(next, end, map, updatedVisited)
-                }
-            }.awaitAll().filterNotNull().minOrNull()
-        }
-    }
-
-    override fun part1(input: List<List<Char>>): Any? {
-        val start = input.findInMatrix { row, col, c ->
-            if (c == start) {
-                Point(y = row, x = col, z = range[c]!!)
-            } else null
+class Day12(input: String) : Day<List<List<Vertex>>>(input) {
+    private val List<List<Vertex>>.start
+        get() = findInMatrix { _, _, vertex ->
+            if (vertex.encodedAltitude == startChar) vertex else null
+        }!!
+    private val List<List<Vertex>>.end
+        get() = findInMatrix { _, _, vertex ->
+            if (vertex.encodedAltitude == endChar) vertex else null
         }!!
 
-        val end = input.findInMatrix { row, col, c ->
-            if (c == end) {
-                Point(y = row, x = col, z = range[c]!!)
-            } else null
-        }!!
+    private fun bfs(start: Vertex, end: Vertex): Int {
+        val queue = ArrayDeque<Pair<Long, Vertex>>()
+        val visited = mutableSetOf<Vertex>()
+        visited.add(start)
+        queue.add(1L to start)
+        var counter = 0
 
-        val map = input.mapIndexed { row, it -> it.mapIndexed { col, c -> Point(y = row, x = col, z = range[c]!!) } }
-
-//        return depth(
-//            Params(
-//                current = start,
-//                end = end,
-//                map = map,
-//                visited = emptySet(),
-//            )
-//        )
-
-        return runBlocking { findEnd(current = start, end = end, map = map, emptySet()) }
+        while (queue.isNotEmpty()) {
+            val (depth, item) = queue.removeFirst()
+            val adj = item.neighbors
+            if (end == item) {
+                return depth.toInt()
+            }
+            adj.forEach {
+                if (!visited.contains(it)) {
+                    if (end == it) {
+                        return depth.toInt()
+                    }
+                    visited.add(it)
+                    queue.add(depth + 1 to it)
+                    counter++
+                }
+            }
+        }
+        return Int.MAX_VALUE
     }
 
-    override fun part2(input: List<List<Char>>): Any? {
-        TODO("Not yet implemented")
+    override fun parseInput(): List<List<Vertex>> {
+        val graph = inputByLines.map { it.toList() }.mapIndexed { rowIndex, row ->
+            row.mapIndexed { colIndex, c ->
+                Vertex(rowIndex, colIndex, c)
+            }
+        }
+        graph.forEach { rows ->
+            rows.forEach { vertex ->
+                vertex.apply {
+                    down = graph.getOrNull(y + 1)?.getOrNull(x)?.pointIsValidHeight(this)
+                    up = graph.getOrNull(y - 1)?.getOrNull(x)?.pointIsValidHeight(this)
+                    left = graph.getOrNull(y)?.getOrNull(x - 1)?.pointIsValidHeight(this)
+                    right = graph.getOrNull(y)?.getOrNull(x + 1)?.pointIsValidHeight(this)
+                }
+            }
+        }
+        return graph
+    }
+
+    override fun part1(input: List<List<Vertex>>): Any {
+        val start = input.start
+        val end = input.end
+        return bfs(start, end)
+    }
+
+    override fun part2(input: List<List<Vertex>>): Any {
+        val end = input.end
+        return input.asSequence().flatten().filter { it.encodedAltitude == 'a' }.map { bfs(it, end) }.min()
+    }
+
+    companion object {
+        const val startChar = 'S'
+        const val endChar = 'E'
+
+        val convertChar: Map<Char, Int> = buildMap {
+            ('a'..'z').forEach {
+                put(it, it - 'a')
+            }
+            // S is same as a
+            put(startChar, get('a')!!)
+            // E is same as z
+            put(endChar, get('z')!!)
+        }
     }
 }
 
